7edabe76453ef9d8a84c19ab82816d04775b5295
xollo.c <xlloew@gmail.com>
2024-12-06 10:32:17 +0900

    Add SettingPopupWidget.h Add SettingPopupWidget.cpp

--- Source/SmartFactory/SettingPopupWidget.cpp
+++ Source/SmartFactory/SettingPopupWidget.cpp
@@ -2,4 +2,82 @@
 
 
 #include "SettingPopupWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+#include "Components/ComboBoxString.h"
+#include "GameFramework/GameUserSettings.h"
 
+void USettingPopupWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	CloseButton = Cast<UButton>(GetWidgetFromName(TEXT("CloseButton")));
+
+	CloseButton->OnClicked.AddDynamic(this, &USettingPopupWidget::CloseButtonCallback);
+
+	if (ResolutionComboBox)
+	{
+		PopulateResolutionOptions();
+
+		ResolutionComboBox->OnSelectionChanged.AddDynamic(this, &USettingPopupWidget::OnResolutionSelected);
+
+	}
+}
+
+void USettingPopupWidget::CloseButtonCallback()
+{
+	RemoveFromParent();
+
+}
+
+void USettingPopupWidget::PopulateResolutionOptions()
+{
+	UGameUserSettings* UserSettings = UGameUserSettings::GetGameUserSettings();
+	if (!UserSettings)
+	{
+		return;
+	}
+
+	TArray<FIntPoint> SupportedResolutions = {
+		FIntPoint(1280, 720),
+		FIntPoint(1920, 1080),
+		FIntPoint(2560, 1440),
+		FIntPoint(3840, 2160)
+	};
+
+	Resolutions = SupportedResolutions;
+
+	for (const FIntPoint& Resolution : Resolutions)
+	{
+		FString ResolutionString = FString::Printf(TEXT("%d x %d"), Resolution.X, Resolution.Y);
+		ResolutionComboBox->AddOption(ResolutionString);
+	}
+
+	FIntPoint CurrentResolution = UserSettings->GetScreenResolution();
+	FString CurrentResolutionString = FString::Printf(TEXT("%d x %d"), CurrentResolution.X, CurrentResolution.Y);
+	ResolutionComboBox->SetSelectedOption(CurrentResolutionString);
+}
+
+void USettingPopupWidget::OnResolutionSelected(FString SelectedOption, ESelectInfo::Type SelectionType)
+{
+	for (const FIntPoint& Resolution : Resolutions)
+	{
+		FString ResolutionString = FString::Printf(TEXT("%d x %d"), Resolution.X, Resolution.Y);
+		if (ResolutionString == SelectedOption)
+		{
+			ApplyResolution(Resolution);
+			break;
+		}
+	}
+}
+
+void USettingPopupWidget::ApplyResolution(FIntPoint Resolution)
+{
+	UGameUserSettings* UserSettings = UGameUserSettings::GetGameUserSettings();
+	if (!UserSettings)
+	{
+		return;
+	}
+	UserSettings->SetScreenResolution(Resolution);
+	UserSettings->ApplySettings(false);
+}
--- Source/SmartFactory/SettingPopupWidget.h
+++ Source/SmartFactory/SettingPopupWidget.h
@@ -6,6 +6,9 @@
 #include "Blueprint/UserWidget.h"
 #include "SettingPopupWidget.generated.h"
 
+
+class UButton;
+class UComboBoxString;
 /**
  * 
  */
@@ -13,5 +16,35 @@ UCLASS()
 class SMARTFACTORY_API USettingPopupWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UButton* CloseButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UComboBoxString* ResolutionComboBox;
+
+
+public:
+
+private:
+	UFUNCTION(BlueprintCallable)
+	void CloseButtonCallback();
 	
+	//해상도 리스트
+	TArray<FIntPoint> Resolutions;
+
+	//ComboBox 초기화
+	void PopulateResolutionOptions();
+
+	//해상도 적용
+	UFUNCTION()
+	void OnResolutionSelected(FString SelectedOption, ESelectInfo::Type SelectionType);
+
+	//해상도 변경
+	void ApplyResolution(FIntPoint Resolution);
+
 };
